ce31bd21a039773f67dc2a8d5ae7b14a68714669
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-25 22:27:59 +0300

    merge input and map, make `map` var as local, call foursquare-search on submit

--- client/views/map.js
+++ client/views/map.js
@@ -1,3 +1,10 @@
+/*
+ * Map template with query input
+ *
+ */
+
+var map;
+
 Template.map.onRendered(function () {
   Mapbox.debug = true;
   Mapbox.load();
@@ -6,7 +13,26 @@ Template.map.onRendered(function () {
     // bind ti div.map mapvox
     if (Mapbox.loaded()) {
       L.mapbox.accessToken = 'pk.eyJ1Ijoib2xleCIsImEiOiIwN2FkZTc5MDI0ODg2NTZiNGRjZjI2NWZjMzhjYWQ0ZiJ9.z2IjKFJX9EMtn_djhbe_7w';
-      var map = L.mapbox.map('map', 'olex.n0b9h12a');
+      map = L.mapbox.map('map', 'olex.n0b9h12a');
     }
   });
+});
+
+Template.input.events({
+  // search query form
+  'submit form': function(e, t) {
+    e.preventDefault();
+    var query = t.find('input').value,
+        location = map.getCenter(),
+        lat = location.lat,
+        lng = location.lng;
+
+    Meteor.call('foursquare-search', lat, lng, query, function(err, venues) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Venues', venues);
+    });
+  }
 });
