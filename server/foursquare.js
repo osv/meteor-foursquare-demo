95ced8dd45db608cb0469b2ef40f3bd6ded1a0ad
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-25 22:12:25 +0300

    Foursquare method

---
+++ server/foursquare.js
@@ -0,0 +1,45 @@
+
+var foursquare_id = process.env.FOURSQUARE_ID,
+    foursquare_secret = process.env.FOURSQUARE_SECRET;
+
+if (! foursquare_secret || ! foursquare_id) {
+  console.log('!! You should set both, FOURSQUARE_ID and FOURSQUARE_SECRET env variables');
+}
+
+Meteor.methods({
+  // return array of venues
+  'foursquare-search': function(lat, lng, query) {
+
+    if (! foursquare_secret || ! foursquare_id) {
+      throw new Meteor.Error('Foursqaure not configured');
+    }
+
+    if(!this.userId) {
+      throw new Meteor.Error('Permission denied');
+    }
+
+    check(lat, Number);
+    check(lng, Number);
+    check(query, String);
+
+    var result,
+        params = {
+          client_id: foursquare_id,
+          client_secret: foursquare_secret,
+          v: 20150606,
+          query: query,
+          ll: '' + lat + ',' + lng,
+    };
+
+    try {
+      result = HTTP.get('https://api.foursquare.com/v2/venues/search', {
+        params: params,
+        timeout: 20000
+      });
+    } catch(error) {
+      throw new Meteor.Error('Foursquare api call failed');
+    }
+
+    return result.data.response.venues;
+  },
+});
